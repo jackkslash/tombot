0241e3024d783b2fa1b95852e91563e968f7daf1
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-08-01 14:39:16 +0000

    reworked message embed logic

--- src/watcher.ts
+++ src/watcher.ts
@@ -33,7 +33,7 @@ export function transactionTracker(client: any) {
       console.log(block.number);
       for (const transaction of block.transactions) {
         logCheck(transaction, Addresses, client);
-        await sleep(500);
+        await sleep(1000);
       }
     } catch (error) {
       console.error(error);
@@ -59,6 +59,7 @@ async function logCheck(transaction: any, Addresses: any, client: any) {
           for (let index = 0; index < log.length; index++) {
             if (log[index].topics[0] == keccak) {
               //and check swap first entry
+              console.log(log[index].topics[0]);
               const contract = new ethers.Contract(
                 log[index].address,
                 ABI,
@@ -87,27 +88,47 @@ async function logCheck(transaction: any, Addresses: any, client: any) {
           const embed = new EmbedBuilder()
             .setTitle(from[0].label + " made a transaction!")
             .setURL("https://etherscan.io/tx/" + tran.transactionHash)
-            .setDescription(
-              "[" +
-                details[0].amount +
-                " " +
-                details[0].token +
-                " (" +
-                details[0].sym +
-                ") ](https://dexscreener.com/ethereum/" +
-                details[0].tokenAddress +
-                ")" +
-                "\n swapped for \n" +
-                "[" +
-                details[1].amount +
-                " " +
-                details[1].token +
-                " (" +
-                details[1].sym +
-                ") ](https://dexscreener.com/ethereum/" +
-                details[1].tokenAddress +
-                ")"
+            .setTimestamp();
+          for (let index = 0; index < details.length; index++) {
+            const tokenAdd = details[index].tokenAddress;
+            const firstSplit = tokenAdd.slice(0, 4);
+            const secondSplit = tokenAdd.slice(-4);
+            const tokenShort = firstSplit + "..." + secondSplit;
+
+            embed.addFields(
+              {
+                name: "Token",
+                value:
+                  "[ " +
+                  details[index].sym +
+                  " ](" +
+                  "https://dexscreener.com/ethereum/" +
+                  details[index].tokenAddress +
+                  ")",
+                inline: true,
+              },
+              { name: "Amount", value: details[index].amount, inline: true },
+              {
+                name: "Address",
+                value:
+                  "[" +
+                  tokenShort +
+                  "](https://etherscan.io/address/" +
+                  details[index].tokenAddress +
+                  ")",
+                inline: true,
+              }
             );
+          }
+
+          if (
+            details[details.length - 1].tokenAddress ==
+            "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+          ) {
+            embed.addFields({ name: "Action", value: "SOLD" });
+          } else {
+            embed.addFields({ name: "Action", value: "BOUGHT" });
+          }
 
           channel.send({ embeds: [embed] });
         } catch (error) {
@@ -126,7 +147,9 @@ async function logCheck(transaction: any, Addresses: any, client: any) {
 //tests a block with a confirmed transaction
 export async function transactionTrackerTest(client: any) {
   console.log("tracker active");
-  const blockNo = 17671938;
+  const blockNo1 = 17842416; // weth into other https://etherscan.io/tx/0x861d9514131067946d83739a9020cb250ee15b75029355a81fa45fa54c16e053
+  const blockNo2 = 17842469; // other into weth https://etherscan.io/tx/0x9037b8e6556ba9a7b4d2a59b068677a1032462f322f4258eb30250a191797dcb
+  const blockNo3 = 17842454; // https://etherscan.io/tx/0x39eea68baaa2aa7e9d6d9846206c658f4565a9c3d49137445947ecc8b9d9b151
   const adds = await Address.aggregate([
     {
       $unwind: "$address",
@@ -142,16 +165,16 @@ export async function transactionTrackerTest(client: any) {
   ]);
   const Addresses = adds[0].address;
   try {
-    const block = await provider.getBlock(blockNo);
+    const block = await provider.getBlock(blockNo1);
     console.log(block.number);
     for (const transaction of block.transactions) {
       let t1 = performance.now();
       logCheck(transaction, Addresses, client);
-      await sleep(500);
+      await sleep(250);
       let t2 = performance.now();
       let elapsed = t2 - t1;
       const time = elapsed + " ms ";
-      console.log(time + "transaction:" + transaction);
+      //console.log(time + "transaction:" + transaction);
     }
   } catch (error) {
     console.error(error);
